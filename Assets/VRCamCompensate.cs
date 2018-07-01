3ac65714777ce24e9dd67b8f83dd3ab0f32bc711
Kikuchi　Takuro <kikuchi.takurou@takenaka.co.jp>
2018-07-01 15:57:38 -0400

    first commit

---
+++ Assets/VRCamCompensate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class VRCamCompensate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        InputTracking.Recenter();
+    }
+
+    /*
+    // Update is called once per frame
+    int count = 0;
+	void Update () {
+        if (count < 30) {
+            //Vector3 pos = InputTracking.GetLocalPosition(XRNode.CenterEye);
+            //transform.position = -pos;
+            //Debug.Log(pos);
+            
+            /*
+            Quaternion rot = InputTracking.GetLocalRotation(XRNode.CenterEye);
+            float xrot = rot.eulerAngles.x;
+            float yrot = rot.eulerAngles.y;
+            float zrot = rot.eulerAngles.z;
+            //transform.localEulerAngles = new Vector3(-xrot,-yrot,-zrot);
+        }
+        count += 1;
+    }
+    */
+}
